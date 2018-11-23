ad3e87111cdad87fd83de25ac990318070073e9c
felisse Alexander <fcalexander@aggies.ncat.edu>
2018-11-22 21:12:25 -0500

    commit first program

---
+++ GoodbyeComp163.java
@@ -0,0 +1,13 @@
+/**
+  * @author tmjenkins@aggies.ncat.edu
+  *
+  * This class prints out the statement "Goodbye COMP 163".
+  */
+  
+ public class GoodbyeComp163 {
+   public static void main(String[] args) {
+   
+      System.out.println("Goodbye COMP 163");
+      
+    }
+  }   
